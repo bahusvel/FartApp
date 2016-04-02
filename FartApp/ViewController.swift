ea78ddac8a06a51e8717f411fa4050ef1d05d362
Denis Lavrov <bahus.vel@gmail.com>
2016-04-03 11:54:34 +1200

    added extra fart sounds, and random fart sound player

--- FartApp/ViewController.swift
+++ FartApp/ViewController.swift
@@ -11,26 +11,50 @@ import AVFoundation
 import WatchConnectivity
 
 class ViewController: UIViewController, WCSessionDelegate {
-	var soundID: SystemSoundID = 0
 	var mainBundle: CFBundleRef = CFBundleGetMainBundle()
 	var session: WCSession?
+	var backgroundPlayers: [AVAudioPlayer] = []
+	var counter = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		if let ref: CFURLRef = CFBundleCopyResourceURL(mainBundle, "fart", "wav", nil) {
-			AudioServicesCreateSystemSoundID(ref, &soundID)
+		do {
+			for url in getFileURLS() {
+				let bp = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: url))
+				bp.prepareToPlay()
+				backgroundPlayers.append(bp)
+			}
+			try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+			try AVAudioSession.sharedInstance().setActive(true)
+			UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+		} catch {
+			print("Player setup failed")
 		}
+		
 		if (WCSession.isSupported()) {
 			session = WCSession.defaultSession()
 			session!.delegate = self;
 			session!.activateSession()
 		}
 		
+		
+	}
+	
+	func getFileURLS() -> [String] {
+		let wavs = NSBundle.mainBundle().pathsForResourcesOfType("wav", inDirectory: "")
+		let mp3s = NSBundle.mainBundle().pathsForResourcesOfType("mp3", inDirectory: "")
+		return wavs + mp3s
+	}
+	
+	func playSound(){
+		backgroundPlayers[counter % backgroundPlayers.count].play()
+		counter += 1
 	}
 	
 	func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
-		AudioServicesPlaySystemSound(soundID)
+		print("Received Here")
+		playSound()
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -39,7 +63,7 @@ class ViewController: UIViewController, WCSessionDelegate {
 	}
 
 	@IBAction func fart(sender: UIButton) {
-		AudioServicesPlaySystemSound(soundID)
+		playSound()
 	}
 
 }
